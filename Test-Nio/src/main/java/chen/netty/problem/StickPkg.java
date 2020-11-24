d179f2b80c8de5b438a8a99b1d840f5bd28e953e
chenwh <chenwh@eastcom-sw.com>
2020-11-24 12:16:21 +0800

    add netty

--- Test-Nio/src/main/java/chen/netty/problem/StickPkg.java
+++ Test-Nio/src/main/java/chen/netty/problem/StickPkg.java
@@ -3,11 +3,11 @@ package chen.netty.problem;
 import chen.netty.CommonClient;
 import chen.netty.CommonServer;
 import chen.netty.Const;
-import chen.utils.NettyBufUtils;
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
@@ -21,35 +21,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class StickPkg {
 
     public static void main(String[] args) {
-        CommonServer commonServer = new CommonServer(new ChannelInboundHandlerAdapter() {
+        CommonServer commonServer = new CommonServer(new LineBasedFrameDecoder(1024),new StringDecoder(),new ChannelInboundHandlerAdapter() {
             private  final AtomicInteger i = new AtomicInteger(0);
 
             @Override
             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                ByteBuf buf = ((ByteBuf) msg);
-                byte[] bytes = new byte[buf.readableBytes()];
-                buf.readBytes(bytes);
-                String receive = new String(bytes, "utf-8");
+                String receive = ((String) msg);
                 if (receive.equals(Const.QUERY)) {
                     log.info("服务端接收到消息次数:{}", i.getAndIncrement());
-                    ctx.writeAndFlush(Unpooled.copiedBuffer(Const.QUERY.getBytes()));
+                    ctx.writeAndFlush(Unpooled.copiedBuffer((Const.QUERY+System.getProperty("line.separator")).getBytes()));
                 }
             }
         });
 
-        CommonClient commonClient = new CommonClient(new ChannelInboundHandlerAdapter() {
+        CommonClient commonClient = new CommonClient(new LineBasedFrameDecoder(1024),new StringDecoder(),new ChannelInboundHandlerAdapter() {
             private  final AtomicInteger i = new AtomicInteger(0);
 
             @Override
             public void channelActive(ChannelHandlerContext ctx) throws Exception {
                 for (int i1 = 0; i1 < 1000; i1++) {
-                    ctx.writeAndFlush(Unpooled.copiedBuffer(Const.QUERY.getBytes()));
+                    ctx.writeAndFlush(Unpooled.copiedBuffer((Const.QUERY+System.getProperty("line.separator")).getBytes()));
                 }
             }
 
             @Override
             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                String read = NettyBufUtils.read(msg);
+                String read = ((String) msg);
                 if (Const.QUERY.equals(read)) {
                     log.info("客户端收到消息次数: {}", i.getAndIncrement());
                 }
