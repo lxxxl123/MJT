bb141eaf61ff2f8d98ac5451c07dbf2c10dcf33a
chenwh <chenwh@eastcom-sw.com>
2020-12-09 09:55:54 +0800

    add netty

--- Test-Spring/readme.md
+++ Test-Spring/readme.md
@@ -4,5 +4,5 @@
 
 ## 功能如下
 - 搭建springboot基础功能
-- 整合swagger-ui 功能 , 访问 localhost:8080/swagger-ui可查看所有接口
+- 整合swagger-ui 功能 , 访问 http://localhost:8080/swagger-ui.html可查看所有接口
 - 通过restful 方式下载文件
--- Test-Spring/src/main/java/org/example/chen/controller/FileTransferController.java
+++ Test-Spring/src/main/java/org/example/chen/controller/FileTransferController.java
@@ -6,25 +6,46 @@ import org.apache.commons.io.FileUtils;
 import org.example.chen.http.HttpUtils;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.OutputStream;
 
 @RestController
 @Slf4j
 public class FileTransferController {
 
     /**
-     * 文件下载
+     * 文件下载2
      * @return
      * @throws Exception
      */
-    @GetMapping("/file/getText")
+    @GetMapping("/file/getText1")
     public ResponseEntity<FileSystemResource> getText() throws  Exception{
         log.info("获取文件");
         File file = new File("./temp/test.txt");
         FileUtils.write(file, "fdafdxcxdsas", "utf-8");
         return HttpUtils.export(file);
+    }
+
+    /**
+     * 文件下载2
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/file/getText2")
+    public void getText(HttpServletResponse response) throws  Exception{
+        log.info("获取文件2");
+        File file = new File("./temp/test.txt");
+        FileUtils.write(file, "fdafdxcxdsas", "utf-8");
+        response.setContentType("application/x-download;");
+        response.setHeader("Content-Disposition", "attachment; filename="+file.getName());
+        try (OutputStream out = response.getOutputStream()) {
+            StreamUtils.copy(FileUtils.openInputStream(file), out);
+        }
+        return;
     }
 }
